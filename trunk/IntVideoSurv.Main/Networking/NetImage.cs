42598f2e9a467231bd9c9e96ac1313d3c4142a12
litaiyong@gmail.com <litaiyong@gmail.com@91b09b76-351b-142d-e547-d081bcca102d>
2011-03-14 14:37:33 +0000

    添加NetImage

---
+++ trunk/IntVideoSurv.Main/Networking/NetImage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace CameraViewer.NetWorking
+{
+    public class NetImage
+    {
+        public Image Image { set; get; }
+        public int CameraId { set; get; }
+    }
+}
