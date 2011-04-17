d985b1c885acffcc1caf7d66685ddd4305ce33a2
litaiyong@gmail.com <litaiyong@gmail.com@91b09b76-351b-142d-e547-d081bcca102d>
2011-04-17 14:01:32 +0000

    增加了一个设置参数CapturePictureFilePath,修改了从long获取时间的问题和CapturePicture.FilePath的生成问题

--- IntVideoSurv.Main/AnalysisXML.cs
+++ IntVideoSurv.Main/AnalysisXML.cs
@@ -35,118 +35,128 @@ namespace CameraViewer
         }
         public void AnalysisFile()
         {
-            int pictureId;
-            int rectId;
-            int vehicleId;
-            int trackId;
-            int faceId;
-            xmlDoc.Load(@"c:\1.0.xml");
-            XmlNodeList xml_vehicles,xml_camera,xml_vehicles_tract_rects,xml_faces;
-
-            xml_camera = xmlDoc.SelectSingleNode("/pr/cameras").ChildNodes;
-            foreach (XmlNode camitem in xml_camera)
+            try
             {
+                int pictureId;
+                int rectId;
+                int vehicleId;
+                int trackId;
+                int faceId;
+                xmlDoc.Load(@"c:\1.0.xml");
+                XmlNodeList xml_vehicles,xml_camera,xml_vehicles_tract_rects,xml_faces;
 
-                XmlElement xcam=(XmlElement)camitem;
-                CapturePicture ocap=new CapturePicture();
-                ocap.CameraID = Convert.ToInt32(xcam.GetAttribute("id"));
-                XmlNode xml_time = xmlDoc.SelectSingleNode("/pr/cameras/camera/timeid");
-                ocap.Datetime = Convert.ToDateTime(xml_time.InnerText);
-                ocap.FilePath="xml/xinxi";
-                pictureId = AnalysisXMLBusiness.Instance.InsertCapturePicture(ref errMessage, ocap);
-                
-                xml_vehicles=xmlDoc.SelectSingleNode("/pr/cameras/camera/objects/vehicles").ChildNodes;
-                foreach (XmlNode veh_item in xml_vehicles)
+                xml_camera = xmlDoc.SelectSingleNode("/pr/cameras").ChildNodes;
+                foreach (XmlNode camitem in xml_camera)
                 {
-                    XmlNode xml_vehicles_rect = xmlDoc.SelectSingleNode("/pr/cameras/camera/objects/vehicles/vehicle/rect");
-                    XmlElement xml_vehicles_rect1 = (XmlElement)xml_vehicles_rect;
-                    REct rect1 = new REct();
-                    rect1.X = Convert.ToInt32(xml_vehicles_rect1.GetAttribute("x"));
-                    rect1.Y = Convert.ToInt32(xml_vehicles_rect1.GetAttribute("y"));
-                    rect1.W = Convert.ToInt32(xml_vehicles_rect1.GetAttribute("w"));
-                    rect1.H = Convert.ToInt32(xml_vehicles_rect1.GetAttribute("h"));
-                    rectId = AnalysisXMLBusiness.Instance.InsertREct(ref errMessage, rect1);
-
-                    Vehicle oveh=new Vehicle();
-                    XmlElement xveh=(XmlElement)veh_item;
-                    oveh.VehicleID=Convert.ToInt32(xveh.GetAttribute("id"));
-                    oveh.platenumber=Convert.ToString(xveh.GetAttribute("platenumber"));
-                    oveh.speed=Convert.ToSingle(xveh.GetAttribute("speed"));
-                    if (Convert.ToInt32(xveh.GetAttribute("stemagainst"))==1)
-                    {
-                        oveh.stemagainst = true;
-                    } 
-                    else
-                    {
-                        oveh.stemagainst = false;
-                    }
-                    if (Convert.ToInt32(xveh.GetAttribute("accident")) == 1)
-                    {
-                        oveh.accident = true;
-                    } 
-                    else
-                    {
-                        oveh.accident = false;
-                    }
-                    if (Convert.ToInt32(xveh.GetAttribute("stop")) == 1)
-                    {
-                        oveh.stop = true;
-                    } 
-                    else
-                    {
-                        oveh.stop = false;
-                    }
-                    if (Convert.ToInt32(xveh.GetAttribute("linechange")) == 1)
-                    {
-                        oveh.linechange = true;
-                    } 
-                    else
-                    {
-                        oveh.linechange = false;
-                    }
-                    oveh.platecolor=Convert.ToString(xveh.GetAttribute("platecolor"));
-                    oveh.vehiclecolor=Convert.ToString(xveh.GetAttribute("vehiclecolor"));
-                    oveh.PictureID = pictureId;
-                    oveh.REctId = rectId;
-                    vehicleId = AnalysisXMLBusiness.Instance.InsertVehicle(ref errMessage, oveh);
-
-                    xml_vehicles_tract_rects = xmlDoc.SelectSingleNode("/pr/cameras/camera/objects/vehicles/vehicle/track").ChildNodes;
-                    foreach (XmlNode xml_vehicles_tract_rects_item in xml_vehicles_tract_rects)
-                    {
-                        REct rect2 = new REct();
-                        XmlElement xml_vehicles_tract_rects_item1 = (XmlElement)xml_vehicles_tract_rects_item;
-                        rect2.X = Convert.ToInt32(xml_vehicles_tract_rects_item1.GetAttribute("x"));
-                        rect2.Y = Convert.ToInt32(xml_vehicles_tract_rects_item1.GetAttribute("y"));
-                        rect2.W = Convert.ToInt32(xml_vehicles_tract_rects_item1.GetAttribute("w"));
-                        rect2.H = Convert.ToInt32(xml_vehicles_tract_rects_item1.GetAttribute("h"));
-                        rectId = AnalysisXMLBusiness.Instance.InsertREct(ref errMessage, rect2);
-                    }
-                    Track track = new Track();
-                    track.REct = rectId;
-                    trackId = AnalysisXMLBusiness.Instance.InsertTrack(ref errMessage, track);
-
-                }
-                xml_faces = xmlDoc.SelectSingleNode("/pr/cameras/camera/objects/faces").ChildNodes;
-                foreach (XmlNode xml_faces_item in xml_faces)
-                {
-                    //XmlElement xml_faces_item1 = (XmlElement)xml_faces_item;
-                    XmlNode xml_faces_item_rect = xmlDoc.SelectSingleNode("/pr/cameras/camera/objects/faces/face/rect");
-                    XmlElement xml_faces_item_rect1 = (XmlElement)xml_faces_item_rect;
-                    REct rect3 = new REct();
-                    rect3.X = Convert.ToInt32(xml_faces_item_rect1.GetAttribute("x"));
-                    rect3.Y = Convert.ToInt32(xml_faces_item_rect1.GetAttribute("y"));
-                    rect3.W = Convert.ToInt32(xml_faces_item_rect1.GetAttribute("w"));
-                    rect3.H = Convert.ToInt32(xml_faces_item_rect1.GetAttribute("h"));
-                    rectId = AnalysisXMLBusiness.Instance.InsertREct(ref errMessage, rect3);
-                    XmlNode xml_faces_item_score = xmlDoc.SelectSingleNode("/pr/cameras/camera/objects/faces/face/score");
-                    Face face = new Face();
-                    face.RectID = rectId;
-                    face.PictureID = pictureId;
-                    face.score = Convert.ToSingle(xml_faces_item_score.InnerText);
-                    faceId = AnalysisXMLBusiness.Instance.InsertFace(ref errMessage, face);
                     
-                }
+                    XmlElement xcam=(XmlElement)camitem;
+                    CapturePicture ocap=new CapturePicture();
+                    ocap.CameraID = Convert.ToInt32(xcam.GetAttribute("id"));
+                    XmlNode xml_time = xmlDoc.SelectSingleNode("/pr/cameras/camera/timeid");
+                    ocap.Datetime = new DateTime(long.Parse(xml_time.InnerText));
+                    ocap.FilePath=Properties.Settings.Default.CapturePictureFilePath +@"\"+ocap.CameraID+
+                        @"\" + ocap.Datetime.ToString(@"yyyy\\MM\\dd\\HH\\") + ocap.CameraID +ocap.Datetime.ToString(@"_yyyy_MM_dd_HH_mm_ss_fff")+".jpg";
+                    pictureId = AnalysisXMLBusiness.Instance.InsertCapturePicture(ref errMessage, ocap);
+                    
+                    xml_vehicles=xmlDoc.SelectSingleNode("/pr/cameras/camera/objects/vehicles").ChildNodes;
+                    foreach (XmlNode veh_item in xml_vehicles)
+                    {
+                        XmlNode xml_vehicles_rect = xmlDoc.SelectSingleNode("/pr/cameras/camera/objects/vehicles/vehicle/rect");
+                        XmlElement xml_vehicles_rect1 = (XmlElement)xml_vehicles_rect;
+                        REct rect1 = new REct();
+                        rect1.X = Convert.ToInt32(xml_vehicles_rect1.GetAttribute("x"));
+                        rect1.Y = Convert.ToInt32(xml_vehicles_rect1.GetAttribute("y"));
+                        rect1.W = Convert.ToInt32(xml_vehicles_rect1.GetAttribute("w"));
+                        rect1.H = Convert.ToInt32(xml_vehicles_rect1.GetAttribute("h"));
+                        rectId = AnalysisXMLBusiness.Instance.InsertREct(ref errMessage, rect1);
 
+                        Vehicle oveh=new Vehicle();
+                        XmlElement xveh=(XmlElement)veh_item;
+                        oveh.VehicleID=Convert.ToInt32(xveh.GetAttribute("id"));
+                        oveh.platenumber=Convert.ToString(xveh.GetAttribute("platenumber"));
+                        oveh.speed=Convert.ToSingle(xveh.GetAttribute("speed"));
+                        if (Convert.ToInt32(xveh.GetAttribute("stemagainst"))==1)
+                        {
+                            oveh.stemagainst = true;
+                        } 
+                        else
+                        {
+                            oveh.stemagainst = false;
+                        }
+                        if (Convert.ToInt32(xveh.GetAttribute("accident")) == 1)
+                        {
+                            oveh.accident = true;
+                        } 
+                        else
+                        {
+                            oveh.accident = false;
+                        }
+                        if (Convert.ToInt32(xveh.GetAttribute("stop")) == 1)
+                        {
+                            oveh.stop = true;
+                        } 
+                        else
+                        {
+                            oveh.stop = false;
+                        }
+                        if (Convert.ToInt32(xveh.GetAttribute("linechange")) == 1)
+                        {
+                            oveh.linechange = true;
+                        } 
+                        else
+                        {
+                            oveh.linechange = false;
+                        }
+                        oveh.platecolor=Convert.ToString(xveh.GetAttribute("platecolor"));
+                        oveh.vehiclecolor=Convert.ToString(xveh.GetAttribute("vehiclecolor"));
+                        oveh.PictureID = pictureId;
+                        oveh.REctId = rectId;
+                        vehicleId = AnalysisXMLBusiness.Instance.InsertVehicle(ref errMessage, oveh);
+
+                        xml_vehicles_tract_rects = xmlDoc.SelectSingleNode("/pr/cameras/camera/objects/vehicles/vehicle/track").ChildNodes;
+                        foreach (XmlNode xml_vehicles_tract_rects_item in xml_vehicles_tract_rects)
+                        {
+                            REct rect2 = new REct();
+                            XmlElement xml_vehicles_tract_rects_item1 = (XmlElement)xml_vehicles_tract_rects_item;
+                            rect2.X = Convert.ToInt32(xml_vehicles_tract_rects_item1.GetAttribute("x"));
+                            rect2.Y = Convert.ToInt32(xml_vehicles_tract_rects_item1.GetAttribute("y"));
+                            rect2.W = Convert.ToInt32(xml_vehicles_tract_rects_item1.GetAttribute("w"));
+                            rect2.H = Convert.ToInt32(xml_vehicles_tract_rects_item1.GetAttribute("h"));
+                            rectId = AnalysisXMLBusiness.Instance.InsertREct(ref errMessage, rect2);
+                        }
+                        Track track = new Track();
+                        track.REct = rectId;
+                        trackId = AnalysisXMLBusiness.Instance.InsertTrack(ref errMessage, track);
+
+                    }
+                    xml_faces = xmlDoc.SelectSingleNode("/pr/cameras/camera/objects/faces").ChildNodes;
+                    foreach (XmlNode xml_faces_item in xml_faces)
+                    {
+                        //XmlElement xml_faces_item1 = (XmlElement)xml_faces_item;
+                        XmlNode xml_faces_item_rect = xmlDoc.SelectSingleNode("/pr/cameras/camera/objects/faces/face/rect");
+                        XmlElement xml_faces_item_rect1 = (XmlElement)xml_faces_item_rect;
+                        REct rect3 = new REct();
+                        rect3.X = Convert.ToInt32(xml_faces_item_rect1.GetAttribute("x"));
+                        rect3.Y = Convert.ToInt32(xml_faces_item_rect1.GetAttribute("y"));
+                        rect3.W = Convert.ToInt32(xml_faces_item_rect1.GetAttribute("w"));
+                        rect3.H = Convert.ToInt32(xml_faces_item_rect1.GetAttribute("h"));
+                        rectId = AnalysisXMLBusiness.Instance.InsertREct(ref errMessage, rect3);
+                        XmlNode xml_faces_item_score = xmlDoc.SelectSingleNode("/pr/cameras/camera/objects/faces/face/score");
+                        Face face = new Face();
+                        face.RectID = rectId;
+                        face.PictureID = pictureId;
+                        face.score = Convert.ToSingle(xml_faces_item_score.InnerText);
+                        faceId = AnalysisXMLBusiness.Instance.InsertFace(ref errMessage, face);
+                        
+                    }
+
+            }
+
+            }
+            catch (Exception ex)
+            {
+                
+                XtraMessageBox.Show(ex.ToString());
             }
 
 
