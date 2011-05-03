2c19e809654ac764266b62c4194496a58699a23d
litaiyong@gmail.com <litaiyong@gmail.com@91b09b76-351b-142d-e547-d081bcca102d>
2011-05-03 10:21:03 +0000

    添加对解码器的命令的任务号的支持：包括数据库和网络组包和解析 新增TaskInfo表： GO /****** 对象:  Table [dbo].[TaskInfo]    脚本日期: 05/03/2011 18:20:29 ******/ SET ANSI_NULLS ON GO SET QUOTED_IDENTIFIER ON GO CREATE TABLE [dbo].[TaskInfo]( 	[TaskId] [int] NOT NULL, 	[CameraId] [int] NOT NULL, 	[DecoderId] [int] NULL, 	[Status] [int] NOT NULL, 	[HappenDateTime] [datetime] NOT NULL ) ON [PRIMARY]

---
+++ IntVideoSurv.DataAccess/TaskDataAccess.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Data;
+using IntVideoSurv.Entity;
+
+namespace IntVideoSurv.DataAccess
+{
+    public class TaskDataAccess
+    {
+        public static bool IsTaskExisted(Database db, int taskId)
+        {
+            string cmdText = string.Format("select count(*) from TaskInfo where TaskId={0}",taskId);
+            try
+            {
+                return int.Parse(db.ExecuteScalar(CommandType.Text, cmdText).ToString())>0;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        public static int Insert(Database db, TaskInfo taskInfo)
+        {
+
+            StringBuilder sbField = new StringBuilder();
+            StringBuilder sbValue = new StringBuilder();
+            sbField.Append("INSERT INTO  TaskInfo(");
+            sbValue.Append("values(");
+            sbField.Append("TaskId");
+            sbValue.AppendFormat("{0}", taskInfo.TaskId);
+            sbField.Append(",CameraId");
+            sbValue.AppendFormat(",{0}", taskInfo.CameraId);
+            sbField.Append(",DecoderId");
+            sbValue.AppendFormat(",{0}", taskInfo.DecoderId);
+            sbField.Append(",Status");
+            sbValue.AppendFormat(",{0}", taskInfo.Status);
+            sbField.Append(",HappenDateTime");
+            sbValue.AppendFormat(",'{0}')", taskInfo.HappenDateTime == null ? DateTime.Now : taskInfo.HappenDateTime);
+            string cmdText = sbField.ToString() + " " + sbValue.ToString();
+            try
+            {
+                cmdText = cmdText.Replace("\r\n", "");
+                return db.ExecuteNonQuery(CommandType.Text, cmdText);
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        public static int Update(Database db, int taskId, int status)
+        {
+
+            StringBuilder sbValue = new StringBuilder();
+            sbValue.Append("update TaskInfo set ");
+
+            sbValue.AppendFormat("Status={0}", status);
+            sbValue.AppendFormat(",HappenDateTime='{0}'", DateTime.Now);
+            sbValue.AppendFormat(" where TaskId={0}", taskId);
+            string cmdText = sbValue.ToString();
+            try
+            {
+                return db.ExecuteNonQuery(CommandType.Text, cmdText);
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+
+        }
+        public static int Delete(Database db, int taskId)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("delete from TaskInfo ");
+            sb.AppendFormat(" where TaskId={0}", taskId);
+            string cmdText = sb.ToString();
+            try
+            {
+                return db.ExecuteNonQuery(CommandType.Text, cmdText);
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
---
+++ IntVideoSurv.Entity/TaskInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Com.SS.Framework.Entity;
+using System.Data;
+using System.Drawing;
+using System.Threading;
+namespace IntVideoSurv.Entity
+{
+    [Serializable]
+    public class TaskInfo : EntityObject
+    {
+        public TaskInfo() { }
+
+        public TaskInfo(IDataReader dataReader) : base(dataReader) { }
+
+        public TaskInfo(DataRow dataRow) : base(dataRow) { }
+
+        [ColumnMapping()]
+        public int TaskId { get; set; }
+
+        [ColumnMapping()]
+        public int CameraId { set; get; }
+        [ColumnMapping()]
+        public int DecoderId { get; set; }
+        [ColumnMapping()]
+        public int Status { get; set; }
+        [ColumnMapping()]
+        public DateTime HappenDateTime { get; set; }
+
+    }
+
+    [Serializable]
+    public enum TaskStatus
+    {
+        StartSend=0,
+        StartSuccess = 1,
+        StartError = 2,
+        Running=4,
+        StopSend=8,
+        StopSuccess = 16,
+        StopError = 32
+    }
+}
