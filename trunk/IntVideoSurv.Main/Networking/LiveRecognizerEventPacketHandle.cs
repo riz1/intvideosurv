653cc66855a759624b47b4a8751d9483c66a565f
litaiyong@gmail.com <litaiyong@gmail.com@91b09b76-351b-142d-e547-d081bcca102d>
2011-05-06 01:53:31 +0000

    添加了识别包的处理流程的说明

--- trunk/IntVideoSurv.Main/Networking/LiveRecognizerEventPacketHandle.cs
+++ trunk/IntVideoSurv.Main/Networking/LiveRecognizerEventPacketHandle.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Text;
 using System.Drawing;
+using System.Xml;
 using log4net;
 
 namespace CameraViewer.NetWorking
@@ -32,10 +33,19 @@ namespace CameraViewer.NetWorking
             {
                 logger.Info("开始解析事件数据");
 
+                //获取xml
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.LoadXml(BitConverter.ToString(bytes, 4));
 
-                //解析事件数据并入库
-                //更新主窗口显示结果的界面
-                OnDataChanged(this, new DataChangeEventArgs(GetType().Name));
+                /*******************处理流程：
+                
+                 * 1）解析xml文件，获取cameraid和DateTime
+                 * 2）根据cameraid和DateTime判断改图像是否已经进入CapturePicture表
+                 * 3）如果是；转5；
+                 * 4）如果否，将改图像从TempPicture表移动到CapturePicture//先获取临时图像GetTempPicture，再移动图像MoveTempPicture
+                 * 5）识别结果入库
+                 */
+
 
                 logger.Info("结束解析事件数据");
             }
@@ -60,6 +70,7 @@ namespace CameraViewer.NetWorking
             for (int i = 0; i < 4; i++)
             {
                 newbyte[i] = header[pos + 3 - i];
+            
             }
             return BitConverter.ToInt32(newbyte, 0);
         }
