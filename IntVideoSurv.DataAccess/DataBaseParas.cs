1cafec6bf08d2b37d4d94d2c85fc55a6dc193bee
litaiyong@gmail.com <litaiyong@gmail.com@91b09b76-351b-142d-e547-d081bcca102d>
2011-05-28 14:35:02 +0000

    添加了一个数据库管理系统类型的enum，方便处理不同数据库的差异，如分页的实现

---
+++ IntVideoSurv.DataAccess/DataBaseParas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntVideoSurv.DataAccess
+{
+    public static class DataBaseParas
+    {
+        public static MyDBType DBType = MyDBType.SqlServer;
+    }
+    public enum MyDBType
+    {
+        Access=1,
+        Mysql=2,
+        SqlServer=4,
+        Oracle=8
+    }
+}
