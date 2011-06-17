9910764d982aec7302bce4e97d90d5ad924728b0
litaiyong@gmail.com <litaiyong@gmail.com@91b09b76-351b-142d-e547-d081bcca102d>
2011-06-17 13:06:21 +0000

    添加了新增操作对oracle的支持，修改了虚拟组中的窗口位置和相关数据库操作上的问题

--- branches/longchang/IntVideoSurv.Business/CameraGroupBusiness.cs
+++ branches/longchang/IntVideoSurv.Business/CameraGroupBusiness.cs
@@ -83,7 +83,7 @@ namespace IntVideoSurv.Business
             {
                 errMessage = ex.Message + ex.StackTrace;
                 logger.Error("Error Message:" + ex.Message + " Trace:" + ex.StackTrace);
-                return null;
+                return new Dictionary<int, CameraInfo>();
             }
         }
         public  int DeleteByGroupIDandCamID(ref string errMessage, int GroupID,int CameraID)
