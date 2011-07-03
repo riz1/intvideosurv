f0166a82cdd3bf9a71214c90bb784cabcfe06d88
litaiyong@gmail.com <litaiyong@gmail.com@91b09b76-351b-142d-e547-d081bcca102d>
2011-07-03 22:35:41 +0000

    frmCaptureLicensexx=>frmCaptureLicensexxNotUsed

--- branches/longchang/IntVideoSurv.Main/frmMain_Win.cs
+++ branches/longchang/IntVideoSurv.Main/frmMain_Win.cs
@@ -2619,7 +2619,7 @@ namespace CameraViewer
 
         private void barButtonItem16_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
         {
-            frmCaptureLicensexx frm = new frmCaptureLicensexx();
+            frmCaptureLicensexxNotUsed frm = new frmCaptureLicensexxNotUsed();
             frm.ShowDialog();
         }
 
