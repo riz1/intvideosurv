09dcd8fc296c9bb401b84ed86e9579b07b868f95
shenbinsc <shenbinsc@91b09b76-351b-142d-e547-d081bcca102d>
2011-07-19 03:35:34 +0000

    基本完成历史记录回放抓拍

---
+++ branches/longchang/IntVideoSurv.Main/Forms/frmHistoryCapture.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace CameraViewer.Forms
+{
+    partial class frmHistoryCapture
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ((System.ComponentModel.ISupportInitialize)(this.videoListContainer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // frmHistoryCapture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1133, 689);
+            this.Name = "frmHistoryCapture";
+            this.Text = "历史视频";
+            ((System.ComponentModel.ISupportInitialize)(this.videoListContainer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+    }
+}
